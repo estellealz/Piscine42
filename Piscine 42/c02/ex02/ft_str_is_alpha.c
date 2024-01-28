a9f21142c1270885c141cb2f5ebe4990479726e4
Estelle_alz <136736604+estellealz@users.noreply.github.com>
2024-01-28 20:43:34 +0100

    Add files via upload

---
+++ Piscine 42/c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,13 @@
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)))
+			return (0);
+		i++;
+	}
+	return (1);
+}
