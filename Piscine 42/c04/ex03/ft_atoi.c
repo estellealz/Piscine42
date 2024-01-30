03fbeaec3e2864f5e868ec37db4dfca11b53554b
Estelle_alz <136736604+estellealz@users.noreply.github.com>
2024-01-30 11:07:12 +0100

    Add files via upload

---
+++ Piscine 42/c04/ex03/ft_atoi.c
@@ -0,0 +1,27 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	n;
+	int	s;
+
+	i = 0;
+	n = 0;
+	s = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			s++;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9' )
+	{
+		n = n * 10 + str[i] - '0';
+		i++;
+	}
+	if (s % 2)
+		return (-n);
+	return (n);
+}
