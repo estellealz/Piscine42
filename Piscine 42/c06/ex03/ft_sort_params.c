4cdf0398444934aaedc7d40ea3007b94d992fb86
Estelle_alz <136736604+estellealz@users.noreply.github.com>
2024-02-02 14:02:08 +0100

    Add files via upload

---
+++ Piscine 42/c06/ex03/ft_sort_params.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		ft_putchar(*(str + i));
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+int		main(int argc, char **argv)
+{
+	int		i1;
+	int		i2;
+	char	*tmp;
+
+	i2 = argc;
+	i1 = 1;
+	while (++i1 <= argc)
+	{
+		i2 = 1;
+		while (++i2 <= argc - 1)
+		{
+			if (ft_strcmp(argv[i2], argv[i2 - 1]) < 0)
+			{
+				tmp = argv[i2];
+				argv[i2] = argv[i2 - 1];
+				argv[i2 - 1] = tmp;
+			}
+		}
+	}
+	i1 = 0;
+	while (++i1 < argc)
+		ft_putstr(argv[i1]);
+}
+
