b5f8e50812a4a9b0e788aa097e328cd00695b8dc
Estelle_alz <136736604+estellealz@users.noreply.github.com>
2024-02-02 14:01:44 +0100

    Add files via upload

---
+++ Piscine 42/c06/ex02/ft_rev_params.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		ft_putchar(*(str + i));
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (++i < argc)
+		ft_putstr(argv[argc - i]);
+}
+
