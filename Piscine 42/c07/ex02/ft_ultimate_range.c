342f3ed7c83616991371f3b3242ca26f95dc7997
Estelle_alz <136736604+estellealz@users.noreply.github.com>
2024-02-03 21:56:09 +0100

    Add files via upload

---
+++ Piscine 42/c07/ex02/ft_ultimate_range.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+int		ft_ultimate_range(int **range, int min, int max)
+{
+	int *tab;
+	int i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	tab = (int*)malloc(sizeof(*tab) * (max - min));
+	if (tab == NULL)
+		return (-1);
+	else
+	{
+		i = -1;
+		while (++i < max - min)
+			tab[i] = min + i;
+		*range = tab;
+		return (i);
+	}
+}
+
