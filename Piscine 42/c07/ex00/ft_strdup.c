214e79a2f2d1aa01a11ebb7740e1b7e694ddd5c2
Estelle_alz <136736604+estellealz@users.noreply.github.com>
2024-02-03 21:55:17 +0100

    Add files via upload

---
+++ Piscine 42/c07/ex00/ft_strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dest = malloc(sizeof(src) * i);
+	i = 0;
+	if (dest == NULL)
+		return (0);
+	else
+	{
+		while (src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+		return (dest);
+	}
+}
+
