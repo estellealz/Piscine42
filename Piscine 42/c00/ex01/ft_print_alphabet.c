51443264124bf06dd9065446458bf75e0f8eb170
Estelle <136736604+estellealz@users.noreply.github.com>
2024-02-15 10:23:14 +0100

    Update ft_print_alphabet.c
    commentaire du code

--- Piscine 42/c00/ex01/ft_print_alphabet.c
+++ Piscine 42/c00/ex01/ft_print_alphabet.c
@@ -1,13 +1,18 @@
 #include <unistd.h>
 
-void	ft_print_alphabet(void)
+// Fonction pour imprimer l'alphabet sur la sortie standard
+void ft_print_alphabet(void)
 {
-	char	i;
+    char i;  // Déclaration d'une variable de type char nommée i
 
-	i = 'a';
-	while (i <= 'z')
-	{
-		write(1, &i, 1);
-		i++;
-	}
+    i = 'a';  // Initialisation de i avec la première lettre de l'alphabet, 'a'
+
+    // Boucle while pour parcourir les lettres de l'alphabet de 'a' à 'z'
+    while (i <= 'z')
+    {
+        // Utilisation de l'appel système write pour écrire le caractère actuel i sur la sortie standard (1)
+        write(1, &i, 1);
+
+        i++;  // Incrémentation de i pour passer à la lettre suivante de l'alphabet
+    }
 }
