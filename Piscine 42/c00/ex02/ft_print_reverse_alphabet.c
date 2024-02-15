354545690f77597c8577f34db4c7ce8288ccea16
Estelle <136736604+estellealz@users.noreply.github.com>
2024-02-15 10:23:51 +0100

    Update ft_print_reverse_alphabet.c
    commentaire du code

--- Piscine 42/c00/ex02/ft_print_reverse_alphabet.c
+++ Piscine 42/c00/ex02/ft_print_reverse_alphabet.c
@@ -1,13 +1,18 @@
 #include <unistd.h>
 
-void	ft_print_reverse_alphabet(void)
+// Fonction pour imprimer l'alphabet en ordre inverse
+void ft_print_reverse_alphabet(void)
 {
-	char	c;
+    char c;  // Déclaration d'une variable de type char nommée c
 
-	c = 'z';
-	while (c >= 'a')
-	{
-		write(1, &c, 1);
-		c--;
-	}
+    c = 'z';  // Initialisation de c avec la dernière lettre de l'alphabet, 'z'
+
+    // Boucle while pour parcourir les lettres de l'alphabet de 'z' à 'a'
+    while (c >= 'a')
+    {
+        // Utilisation de l'appel système write pour écrire le caractère actuel c sur la sortie standard (1)
+        write(1, &c, 1);
+
+        c--;  // Décrémentation de c pour passer à la lettre précédente de l'alphabet
+    }
 }
