e8b9c214b493b58cec7425e4456b647b465b2eb0
Estelle <136736604+estellealz@users.noreply.github.com>
2024-02-15 10:24:30 +0100

    Update ft_print_numbers.c
    commentaire du code

--- Piscine 42/c00/ex03/ft_print_numbers.c
+++ Piscine 42/c00/ex03/ft_print_numbers.c
@@ -1,13 +1,18 @@
 #include <unistd.h>
 
-void	ft_print_numbers(void)
+// Fonction pour imprimer les chiffres de 0 à 9
+void ft_print_numbers(void)
 {
-	char	c;
+    char c;  // Déclaration d'une variable de type char nommée c
 
-	c = '0';
-	while (c <= '9')
-	{
-		write(1, &c, 1);
-		c++;
-	}
+    c = '0';  // Initialisation de c avec le premier chiffre, '0'
+
+    // Boucle while pour parcourir les chiffres de 0 à 9
+    while (c <= '9')
+    {
+        // Utilisation de l'appel système write pour écrire le caractère actuel c sur la sortie standard (1)
+        write(1, &c, 1);
+
+        c++;  // Incrémentation de c pour passer au chiffre suivant
+    }
 }
