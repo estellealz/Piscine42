12a6307eb59b03ce0866f6dc2d17174e79d82f2c
Estelle <136736604+estellealz@users.noreply.github.com>
2024-02-16 14:38:31 +0100

    Update ft_print_comb.c

--- Piscine 42/c00/ex05/ft_print_comb.c
+++ Piscine 42/c00/ex05/ft_print_comb.c
@@ -1,52 +1,65 @@
 #include <unistd.h>
 
-void	increment(char *combo);
-void	print_numbers(char *combo);
-void	ft_putchar(char c);
+void increment(char *combo);
+void print_numbers(char *combo);
+void ft_putchar(char c);
 
-void	ft_print_comb(void)
+void ft_print_comb(void)
 {
-	char	combo[3];
+    char combo[3];
 
-	combo[0] = '0';
-	combo[1] = '1';
-	combo[2] = '2';
-	while (combo[0] <= '7')
-	{
-		print_numbers(combo);
-		increment(combo);
-	}
+    // Initialisation de la première combinaison
+    combo[0] = '0';
+    combo[1] = '1';
+    combo[2] = '2';
+
+    // Boucle principale pour générer et imprimer les combinaisons
+    while (combo[0] <= '7')
+    {
+        // Imprimer la combinaison actuelle
+        print_numbers(combo);
+
+        // Incrémenter vers la prochaine combinaison
+        increment(combo);
+    }
 }
 
-void	increment(char *combo)
+// Fonction pour incrémenter la combinaison actuelle
+void increment(char *combo)
 {
-	combo[2]++;
-	if (combo[2] > '9')
-	{
-		combo[1]++;
-		combo[2] = combo[1] + 1;
-		if (combo[1] > '8')
-		{
-			combo[0]++;
-			combo[1] = combo[0] + 1;
-			combo[2] = combo[1] + 1;
-		}
-	}
+    combo[2]++; // Incrémenter l'unité
+
+    if (combo[2] > '9')
+    {
+        combo[1]++; // Passer à la dizaine
+        combo[2] = combo[1] + 1; // Réinitialiser l'unité
+
+        if (combo[1] > '8')
+        {
+            combo[0]++; // Passer à la centaine
+            combo[1] = combo[0] + 1; // Réinitialiser la dizaine
+            combo[2] = combo[1] + 1; // Réinitialiser l'unité
+        }
+    }
 }
 
-void	print_numbers(char *combo)
+// Fonction pour imprimer la combinaison actuelle
+void print_numbers(char *combo)
 {
-	ft_putchar(combo[0]);
-	ft_putchar(combo[1]);
-	ft_putchar(combo[2]);
-	if (combo[0] < '7')
-	{
-		ft_putchar(',');
-		ft_putchar(' ');
-	}
+    ft_putchar(combo[0]);
+    ft_putchar(combo[1]);
+    ft_putchar(combo[2]);
+
+    // Imprimer la virgule et l'espace si ce n'est pas la dernière combinaison
+    if (combo[0] < '7')
+    {
+        ft_putchar(',');
+        ft_putchar(' ');
+    }
 }
 
-void	ft_putchar(char c)
+// Fonction pour imprimer un caractère
+void ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
