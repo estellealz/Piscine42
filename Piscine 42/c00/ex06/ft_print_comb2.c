f1479cc6a50576f9683b02e74b1a810400549bce
Estelle <136736604+estellealz@users.noreply.github.com>
2024-02-16 14:40:02 +0100

    Update ft_print_comb2.c

--- Piscine 42/c00/ex06/ft_print_comb2.c
+++ Piscine 42/c00/ex06/ft_print_comb2.c
@@ -1,57 +1,68 @@
 #include <unistd.h>
 
-void	print_2digits(char *combo, int n);
-void	ft_putchar(char c);
-void	increment(char *combo);
+void print_2digits(char *combo, int n);
+void ft_putchar(char c);
+void increment(char *combo);
 
-void	ft_print_comb2(void)
+void ft_print_comb2(void)
 {
-	char	combo[2];
+    char combo[2];
 
-	combo[0] = 0;
-	combo[1] = 1;
-	while (combo[0] < 99)
-	{
-		print_2digits(combo, 0);
-		ft_putchar(' ');
-		print_2digits(combo, 1);
-		if (combo[0] < 98)
-		{
-			ft_putchar(',');
-			ft_putchar(' ');
-		}
-		increment(combo);
-	}
+    combo[0] = 0;
+    combo[1] = 1;
+    
+    // Boucle principale pour parcourir toutes les paires
+    while (combo[0] < 99)
+    {
+        // Imprimer les deux chiffres de la paire
+        print_2digits(combo, 0);
+        ft_putchar(' ');
+        print_2digits(combo, 1);
+
+        // Ajouter une virgule et un espace, sauf pour la dernière paire
+        if (combo[0] < 98)
+        {
+            ft_putchar(',');
+            ft_putchar(' ');
+        }
+
+        // Incrémenter vers la paire suivante
+        increment(combo);
+    }
 }
 
-void	print_2digits(char *combo, int n)
+// Imprimer les deux chiffres de la paire actuelle
+void print_2digits(char *combo, int n)
 {
-	char	ascii_offset;
+    char ascii_offset = 48; // Valeur ASCII de '0'
 
-	ascii_offset = 48;
-	if (combo[n] < 10)
-	{
-		ft_putchar('0');
-		ft_putchar(combo[n] + ascii_offset);
-	}	
-	else
-	{
-		ft_putchar(combo[n] / 10 + ascii_offset);
-		ft_putchar(combo[n] % 10 + ascii_offset);
-	}
+    if (combo[n] < 10)
+    {
+        // Si le chiffre est inférieur à 10, ajouter un zéro avant
+        ft_putchar('0');
+        ft_putchar(combo[n] + ascii_offset);
+    }
+    else
+    {
+        // Sinon, imprimer les deux chiffres
+        ft_putchar(combo[n] / 10 + ascii_offset);
+        ft_putchar(combo[n] % 10 + ascii_offset);
+    }
 }
 
-void	ft_putchar(char c)
+// Incrémenter la paire vers la suivante
+void increment(char *combo)
 {
-	write(1, &c, 1);
+    combo[1]++;
+    if (combo[1] > 99)
+    {
+        combo[0]++;
+        combo[1] = combo[0] + 1;
+    }
 }
 
-void	increment(char *combo)
+// Imprimer un caractère
+void ft_putchar(char c)
 {
-	combo[1]++;
-	if (combo[1] > 99)
-	{
-		combo[0]++;
-		combo[1] = combo[0] + 1;
-	}
+    write(1, &c, 1);
 }
