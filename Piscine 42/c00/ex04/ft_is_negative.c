33daf60eab45214f049401576c76792bd200fad8
Estelle <136736604+estellealz@users.noreply.github.com>
2024-02-16 14:37:48 +0100

    Update ft_is_negative.c

--- Piscine 42/c00/ex04/ft_is_negative.c
+++ Piscine 42/c00/ex04/ft_is_negative.c
@@ -1,18 +1,23 @@
 #include <unistd.h>
 
-void	ft_is_negative(int n)
+// Fonction pour déterminer si un nombre est négatif ou non
+void ft_is_negative(int n)
 {
-	char	cp;
-	char	cn;
+    char cp;  // Caractère pour représenter 'Positif'
+    char cn;  // Caractère pour représenter 'Négatif'
 
-	cp = 'P';
-	cn = 'N';
-	if (n >= 0)
-	{
-		write(1, &cp, 1);
-	}
-	else
-	{
-		write(1, &cn, 1);
-	}
+    cp = 'P';  // Initialisation avec le caractère 'P' pour Positif
+    cn = 'N';  // Initialisation avec le caractère 'N' pour Négatif
+
+    // Vérification si n est supérieur ou égal à zéro
+    if (n >= 0)
+    {
+        // Utilisation de l'appel système write pour écrire 'P' sur la sortie standard (1)
+        write(1, &cp, 1);
+    }
+    else
+    {
+        // Utilisation de l'appel système write pour écrire 'N' sur la sortie standard (1)
+        write(1, &cn, 1);
+    }
 }
