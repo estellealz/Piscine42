c8b5d2fe42afacbbe3f15a5c3de54aa1ae0bb6cd
Estelle <136736604+estellealz@users.noreply.github.com>
2024-02-16 14:41:00 +0100

    Update ft_putnbr.c

--- Piscine 42/c00/ex07/ft_putnbr.c
+++ Piscine 42/c00/ex07/ft_putnbr.c
@@ -1,68 +1,70 @@
 #include <unistd.h>
 
-void	ft_print_reverse_string(char *string);
-void	ft_putchar(char c);
-int		check_number(int nb, long *num_temp, char *out_string, char *c_char);
+void ft_print_reverse_string(char *string);
+void ft_putchar(char c);
+int check_number(int nb, long *num_temp, char *out_string, char *c_char);
 
-void	ft_putnbr(int nb)
+void ft_putnbr(int nb)
 {
-	char	number_string [12];
-	char	c_char;
-	char	ascii_offset;
-	int		pos_in_string;
-	long	n_temp;
+    char number_string[12];
+    char c_char;
+    char ascii_offset = 48;
+    int pos_in_string;
+    long n_temp;
 
-	c_char = '\0';
-	ascii_offset = 48;
-	n_temp = nb;
-	pos_in_string = check_number(nb, &n_temp, number_string, &c_char);
-	while (n_temp > 0)
-	{
-		number_string[pos_in_string] = (char)(n_temp % 10l) + ascii_offset;
-		pos_in_string++;
-		n_temp = n_temp / 10l;
-	}
-	number_string[pos_in_string] = c_char;
-	number_string[pos_in_string + 1] = '\0';
-	ft_print_reverse_string(number_string);
+    c_char = '\0';
+    n_temp = nb;
+    pos_in_string = check_number(nb, &n_temp, number_string, &c_char);
+
+    while (n_temp > 0)
+    {
+        number_string[pos_in_string] = (char)(n_temp % 10l) + ascii_offset;
+        pos_in_string++;
+        n_temp = n_temp / 10l;
+    }
+
+    number_string[pos_in_string] = c_char;
+    number_string[pos_in_string + 1] = '\0';
+    ft_print_reverse_string(number_string);
 }
 
-int	check_number(int nb, long *num_temp, char *out_string, char *c_char)
+int check_number(int nb, long *num_temp, char *out_string, char *c_char)
 {
-	int	output;
+    int output = 0;
 
-	output = 0;
-	if (nb < 0)
-	{
-		*c_char = '-';
-		*num_temp = *num_temp * (-1l);
-	}
-	else if (nb == 0)
-	{
-		out_string[0] = '0';
-		output++;
-	}
-	return (output);
+    if (nb < 0)
+    {
+        *c_char = '-';
+        *num_temp = *num_temp * (-1l);
+    }
+    else if (nb == 0)
+    {
+        out_string[0] = '0';
+        output++;
+    }
+
+    return output;
 }
 
-void	ft_print_reverse_string(char *string)
+void ft_print_reverse_string(char *string)
 {
-	int	i;
+    int i = 0;
 
-	i = 0;
-	while (string[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (i >= 0)
-	{
-		ft_putchar(string[i]);
-		i--;
-	}
+    while (string[i] != '\0')
+    {
+        i++;
+    }
+
+    i--;
+
+    while (i >= 0)
+    {
+        ft_putchar(string[i]);
+        i--;
+    }
 }
 
-void	ft_putchar(char c)
+void ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
