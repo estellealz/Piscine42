e2a4716506155c488d0ece91b7613baed88724f6
Estelle <136736604+estellealz@users.noreply.github.com>
2024-02-16 14:42:09 +0100

    Update ft_print_combn.c

--- Piscine 42/c00/ex08/ft_print_combn.c
+++ Piscine 42/c00/ex08/ft_print_combn.c
@@ -1,74 +1,71 @@
 #include <unistd.h>
 
-void	increment(char *combo, int n);
-void	increment_recursive(char *combo, int k, int i);
-void	print_numbers(char *combo, int n);
-void	ft_putchar(char c);
+void increment(char *combo, int n);
+void increment_recursive(char *combo, int k, int i);
+void print_numbers(char *combo, int n);
+void ft_putchar(char c);
 
-void	ft_print_combn(int n)
+void ft_print_combn(int n)
 {
-	char	combo[9];
-	int		i;
+    char combo[9];
+    int i = 0;
 
-	i = 0;
-	while (i < n)
-	{
-		combo[i] = (char) i + 48;
-		i++;
-	}
-	print_numbers(combo, n);
-	while (combo[0] <= '9' - n || combo[n - 1] < '9')
-	{
-		increment(combo, n);
-		print_numbers(combo, n);
-	}
+    while (i < n)
+    {
+        combo[i] = (char)i + 48;
+        i++;
+    }
+    print_numbers(combo, n);
+
+    while (combo[0] <= '9' - n || combo[n - 1] < '9')
+    {
+        increment(combo, n);
+        print_numbers(combo, n);
+    }
 }
 
-void	increment(char *combo, int n)
+void increment(char *combo, int n)
 {
-	int	i;
-	int	k;
+    int i;
+    int k = 0;
 
-	k = 0;
-	i = n - 1;
-	increment_recursive(combo, k, i);
+    i = n - 1;
+    increment_recursive(combo, k, i);
 }
 
-void	increment_recursive(char *combo, int k, int i)
+void increment_recursive(char *combo, int k, int i)
 {
-	if (combo[i - k] < '9' - k)
-	{
-		combo[i - k]++;
-	}
-	else
-	{
-		k++;
-		if (k <= i)
-		{
-			increment_recursive(combo, k, i);
-			combo[i - k + 1] = combo[i - k] + 1;
-		}
-	}
+    if (combo[i - k] < '9' - k)
+    {
+        combo[i - k]++;
+    }
+    else
+    {
+        k++;
+        if (k <= i)
+        {
+            increment_recursive(combo, k, i);
+            combo[i - k + 1] = combo[i - k] + 1;
+        }
+    }
 }
 
-void	print_numbers(char *combo, int n)
+void print_numbers(char *combo, int n)
 {
-	int	i;
-
-	i = 0;
-	while (i < n)
-	{
-		ft_putchar(combo[i]);
-		i++;
-	}
-	if (combo[0] <= '9' - n && combo[n - 1] <= '9')
-	{
-		ft_putchar(',');
-		ft_putchar(' ');
-	}
+    int i = 0;
+    while (i < n)
+    {
+        ft_putchar(combo[i]);
+        i++;
+    }
+    if (combo[0] <= '9' - n && combo[n - 1] <= '9')
+    {
+        ft_putchar(',');
+        ft_putchar(' ');
+    }
 }
 
-void	ft_putchar(char c)
+void ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
