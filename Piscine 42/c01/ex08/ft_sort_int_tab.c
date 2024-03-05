fb2af08f0a34fc2c684d30d8676af39110fac614
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-05 09:42:37 +0100

    Update ft_sort_int_tab.c

--- Piscine 42/c01/ex08/ft_sort_int_tab.c
+++ Piscine 42/c01/ex08/ft_sort_int_tab.c
@@ -1,35 +1,34 @@
 #include <unistd.h>
 
-void	ft_swap(int *a, int *b)
+// Fonction pour échanger les valeurs de deux entiers
+void ft_swap(int *a, int *b)
 {
-	int	c;
-
-	c = *a;
-	*a = *b;
-	*b = c;
+    int c = *a;
+    *a = *b;
+    *b = c;
 }
 
-void	ft_sort_int_tab(int *tab, int size)
+// Fonction pour trier un tableau d'entiers par ordre décroissant
+void ft_sort_int_tab(int *tab, int size)
 {
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (i < size)
-	{
-		while (j <= size + 1)
-		{
-			if (j < size + 1)
-			{
-				if (tab[j] > tab[j - 1])
-				{
-					ft_swap(&tab[j], &tab[j - 1]);
-				}
-			}
-			j++;
-		}
-		j = 0;
-		i++;
-	}
+    int i = 0;
+    int j = 0;
+    
+    // Parcourt le tableau
+    while (i < size)
+    {
+        // Parcourt le tableau à partir de la position i
+        while (j < size)
+        {
+            // Vérifie si l'élément à la position j est plus grand que l'élément à la position i
+            if (tab[j] > tab[i])
+            {
+                // Appelle la fonction ft_swap pour échanger les éléments non triés
+                ft_swap(&tab[i], &tab[j]);
+            }
+            j++;
+        }
+        j = 0;
+        i++;
+    }
 }
