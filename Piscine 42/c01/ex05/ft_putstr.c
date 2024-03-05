1eae4b5911e2579bb0b170a0872939d347d5662d
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-05 09:40:21 +0100

    Update ft_putstr.c

--- Piscine 42/c01/ex05/ft_putstr.c
+++ Piscine 42/c01/ex05/ft_putstr.c
@@ -1,18 +1,19 @@
 #include <unistd.h>
 
-void	ft_putchar(char c)
+// Fonction pour afficher un caractère
+void ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
 
-void	ft_putstr(char *str)
+// Fonction pour afficher une chaîne de caractères
+void ft_putstr(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
+    int i = 0;
+    // Parcours de la chaîne de caractères jusqu'au caractère nul '\0'
+    while (str[i])
+    {
+        ft_putchar(str[i]); // Affichage du caractère actuel
+        i++;
+    }
 }
