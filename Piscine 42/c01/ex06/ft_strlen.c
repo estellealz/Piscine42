86bbb819f2fcabb1e7bd7a337f7fdc83b8faca6f
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-05 09:40:49 +0100

    Update ft_strlen.c

--- Piscine 42/c01/ex06/ft_strlen.c
+++ Piscine 42/c01/ex06/ft_strlen.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
-int	ft_strlen(char *str)
+// Fonction pour calculer la longueur d'une chaîne de caractères
+int ft_strlen(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+    int i = 0;
+    // Parcours de la chaîne de caractères jusqu'au caractère nul '\0'
+    while (str[i])
+        i++;
+    return (i); // Retourne le nombre de caractères dans la chaîne
 }
