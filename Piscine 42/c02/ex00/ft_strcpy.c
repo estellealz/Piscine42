6d3748debd0b61291a8ff0e12f6ca0ee55e9776c
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-07 14:43:26 +0100

    Update ft_strcpy.c

--- Piscine 42/c02/ex00/ft_strcpy.c
+++ Piscine 42/c02/ex00/ft_strcpy.c
@@ -1,13 +1,21 @@
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
+#include <unistd.h>
 
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+// Fonction pour copier une chaîne de caractères source dans une destination
+char *ft_strcpy(char *dest, char *src)
+{
+    int i = 0;
+    
+    // Parcourt la chaîne source
+    while (src[i] != '\0')
+    {
+        // Copie chaque caractère de la source vers la destination
+        dest[i] = src[i];
+        i++;
+    }
+    
+    // Ajoute le caractère de fin de chaîne à la destination
+    dest[i] = '\0';
+    
+    // Renvoie la chaîne destination
+    return dest;
 }
