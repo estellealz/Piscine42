ef23500fe01e63dba7269082547091bf9cf60e4d
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-07 14:44:51 +0100

    Update ft_str_is_numeric.c

--- Piscine 42/c02/ex03/ft_str_is_numeric.c
+++ Piscine 42/c02/ex03/ft_str_is_numeric.c
@@ -1,13 +1,16 @@
-int	ft_str_is_numeric(char *str)
+// Fonction pour vérifier si tous les caractères d'une chaîne sont numériques
+int ft_str_is_numeric(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (!(str[i] >= 48 && str[i] <= 57))
-			return (0);
-		i++;
-	}
-	return (1);
+    int i = 0;
+    
+    // Parcourt la chaîne
+    while (str[i] != '\0')
+    {
+        // Vérifie si le caractère n'est pas un chiffre (0-9)
+        if (!(str[i] >= '0' && str[i] <= '9'))
+            return 0; // Si un caractère n'est pas numérique, renvoie 0
+        i++;
+    }
+    
+    return 1; // Si tous les caractères sont numériques, renvoie 1
 }
