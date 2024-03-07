f116bc4a3c68e219ca285abc0f965c1dceb8e01b
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-07 14:45:52 +0100

    Update ft_str_is_uppercase.c

--- Piscine 42/c02/ex05/ft_str_is_uppercase.c
+++ Piscine 42/c02/ex05/ft_str_is_uppercase.c
@@ -1,13 +1,16 @@
-int	ft_str_is_uppercase(char *str)
+// Fonction pour vérifier si tous les caractères d'une chaîne sont en majuscules
+int ft_str_is_uppercase(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (!(str[i] >= 65 && str[i] <= 90))
-			return (0);
-		i++;
-	}
-	return (1);
+    int i = 0;
+    
+    // Parcourt la chaîne
+    while (str[i] != '\0')
+    {
+        // Vérifie si le caractère n'est pas une lettre majuscule
+        if (!(str[i] >= 'A' && str[i] <= 'Z'))
+            return 0; // Si un caractère n'est pas en majuscule, renvoie 0
+        i++;
+    }
+    
+    return 1; // Si tous les caractères sont en majuscules, renvoie 1
 }
