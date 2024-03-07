ac0bb8c981ce4f815fac138b5742a2564e1e5b58
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-07 14:43:54 +0100

    Update ft_strncpy.c

--- Piscine 42/c02/ex01/ft_strncpy.c
+++ Piscine 42/c02/ex01/ft_strncpy.c
@@ -1,17 +1,25 @@
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int	i;
+#include <unistd.h>
 
-	i = 0;
-	while (src[i] != '\0' && i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
+// Fonction pour copier les n premiers caractères d'une chaîne source dans une destination
+char *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i = 0;
+    
+    // Parcourt la chaîne source jusqu'à n caractères ou la fin de la chaîne
+    while (src[i] != '\0' && i < n)
+    {
+        // Copie chaque caractère de la source vers la destination
+        dest[i] = src[i];
+        i++;
+    }
+    
+    // Ajoute des caractères nuls supplémentaires à la destination si nécessaire
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    
+    // Renvoie la chaîne destination
+    return dest;
 }
