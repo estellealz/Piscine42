47a731ab2795ad589e1ce1732e065553e92d1040
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-07 14:45:18 +0100

    Update ft_str_is_lowercase.c

--- Piscine 42/c02/ex04/ft_str_is_lowercase.c
+++ Piscine 42/c02/ex04/ft_str_is_lowercase.c
@@ -1,13 +1,16 @@
-int	ft_str_is_lowercase(char *str)
+// Fonction pour vérifier si tous les caractères d'une chaîne sont en minuscules
+int ft_str_is_lowercase(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (!(str[i] >= 97 && str[i] <= 122))
-			return (0);
-		i++;
-	}
-	return (1);
+    int i = 0;
+    
+    // Parcourt la chaîne
+    while (str[i] != '\0')
+    {
+        // Vérifie si le caractère n'est pas une lettre minuscule
+        if (!(str[i] >= 'a' && str[i] <= 'z'))
+            return 0; // Si un caractère n'est pas en minuscule, renvoie 0
+        i++;
+    }
+    
+    return 1; // Si tous les caractères sont en minuscules, renvoie 1
 }
