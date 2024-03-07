5acc85b7af4d93ca625db43b407a24083320ca55
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-07 14:44:24 +0100

    Update ft_str_is_alpha.c

--- Piscine 42/c02/ex02/ft_str_is_alpha.c
+++ Piscine 42/c02/ex02/ft_str_is_alpha.c
@@ -1,13 +1,16 @@
-int	ft_str_is_alpha(char *str)
+// Fonction pour vérifier si tous les caractères d'une chaîne sont alphabétiques
+int ft_str_is_alpha(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (!((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)))
-			return (0);
-		i++;
-	}
-	return (1);
+    int i = 0;
+    
+    // Parcourt la chaîne
+    while (str[i] != '\0')
+    {
+        // Vérifie si le caractère n'est pas une lettre majuscule (A-Z) ni une lettre minuscule (a-z)
+        if (!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')))
+            return 0; // Si un caractère n'est pas alphabétique, renvoie 0
+        i++;
+    }
+    
+    return 1; // Si tous les caractères sont alphabétiques, renvoie 1
 }
