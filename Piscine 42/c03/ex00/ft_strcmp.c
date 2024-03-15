26995c6629c51ac68b5f683083c780d75d4209df
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-15 09:57:58 +0100

    Update ft_strcmp.c
    ajout de commentaires

--- Piscine 42/c03/ex00/ft_strcmp.c
+++ Piscine 42/c03/ex00/ft_strcmp.c
@@ -1,21 +1,21 @@
-int	ft_strcmp(char *s1, char *s2)
+int ft_strcmp(char *s1, char *s2)
 {
-	int				i;
-	char			diff;
-	unsigned char	*s1u;
-	unsigned char	*s2u;
+    int i;  // Déclaration d'un compteur pour parcourir les chaînes
+    char diff;  // Variable pour stocker la différence entre les caractères
+    unsigned char *s1u;  // Pointeur vers la première chaîne en tant que tableau d'octets non signés
+    unsigned char *s2u;  // Pointeur vers la deuxième chaîne en tant que tableau d'octets non signés
 
-	s1u = (unsigned char *) s1;
-	s2u = (unsigned char *) s2;
-	i = 0;
-	diff = 0;
-	while (s1u[i] && s2u[i])
-	{
-		diff = s1u[i] - s2u[i];
-		i++;
-		if (diff != 0)
-			return (diff);
-	}
-	diff = s1u[i] - s2u[i];
-	return (diff);
+    s1u = (unsigned char *) s1;  // Conversion de la première chaîne en tableau d'octets non signés
+    s2u = (unsigned char *) s2;  // Conversion de la deuxième chaîne en tableau d'octets non signés
+    i = 0;  // Initialisation du compteur
+    diff = 0;  // Initialisation de la variable de différence à zéro
+    while (s1u[i] && s2u[i])  // Parcourt les deux chaînes jusqu'à ce qu'on atteigne la fin de l'une d'entre elles
+    {
+        diff = s1u[i] - s2u[i];  // Calcule la différence entre les caractères actuels des deux chaînes
+        i++;  // Incrémente le compteur pour passer au caractère suivant
+        if (diff != 0)  // Si la différence est non nulle, cela signifie que les chaînes sont différentes à cette position
+            return (diff);  // Renvoie la différence entre les caractères
+    }
+    diff = s1u[i] - s2u[i];  // Si on atteint la fin d'une des chaînes, calcule la différence entre les caractères terminaux
+    return (diff);  // Renvoie la différence entre les caractères
 }
