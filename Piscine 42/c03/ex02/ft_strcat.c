d84c338ae1320139a35c574e112bec5629a9b6ac
Estelle <136736604+estellealz@users.noreply.github.com>
2024-03-15 09:59:27 +0100

    Update ft_strcat.c
    ajout de commentaires

--- Piscine 42/c03/ex02/ft_strcat.c
+++ Piscine 42/c03/ex02/ft_strcat.c
@@ -1,17 +1,17 @@
-char	*ft_strcat(char *dest, char *src)
+char *ft_strcat(char *dest, char *src)
 {
-	int	i;
-	int	j;
+    int i;  // Déclaration d'un compteur pour parcourir dest
+    int j;  // Déclaration d'un compteur pour parcourir src
 
-	i = 0;
-	while (dest[i])
-		i++;
-	j = 0;
-	while (src[j])
-	{
-		dest[i + j] = src[j];
-		j++;
-	}
-	dest[i + j] = '\0';
-	return (dest);
+    i = 0;  // Initialisation du compteur pour dest
+    while (dest[i])
+        i++;  // Trouve la fin de la chaîne dest
+    j = 0;  // Initialisation du compteur pour src
+    while (src[j])
+    {
+        dest[i + j] = src[j];  // Copie chaque caractère de src à la fin de dest
+        j++;
+    }
+    dest[i + j] = '\0';  // Ajoute le caractère de fin de chaîne à la nouvelle fin de dest
+    return (dest);  // Renvoie dest, maintenant concaténée avec src
 }
